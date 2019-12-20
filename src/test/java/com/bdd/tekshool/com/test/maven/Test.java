a4eb21d5db6f0baf08f1f812999f59e0c02ffe63
SuhbatKhan <khan.suhbat@gmail.com>
2019-12-19 20:19:25 -0500

    this is america

---
+++ src/test/java/com/bdd/tekshool/com/test/maven/Test.java
@@ -0,0 +1,13 @@
+package com.bdd.tekshool.com.test.maven;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is test");
+		
+
+	}
+
+}
